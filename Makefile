d87b1694f7c335f9452ecfaaf218fb6d0081cb54
TheAyeshaSiddiqa <ayeshasiddiqatheprogrammer@gmail.com>
2020-10-19 14:44:18 +0500

    Resolve conflicts

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 test:
-	docker-compose -f local.yml run --rm test python -m pytest
+	docker-compose -f local.yml run --rm app python -m pytest
 build:
 	docker-compose -f local.yml build
 
--- tests/repositories/test_s3_enrolment_repo.py
+++ tests/repositories/test_s3_enrolment_repo.py
@@ -5,10 +5,10 @@ The are testing the encapsulation of the "impure" code
 the repos should return pure domain objects
 of the appropriate type.
 """
-from os import environ
 from unittest.mock import patch
 from uuid import UUID
 
+from app.config import settings
 from app.repositories.s3_enrolment_repo import S3EnrolmentRepo
 
 
@@ -30,7 +30,7 @@ def test_save_enrolment(boto_client, uuid4):
     """
     uuid4.return_value = UUID("1dad3dd8-af28-4e61-ae23-4c93a456d10e")
     repo = S3EnrolmentRepo()
-    environ["ENROLMENT_AUTHORISATION_BUCKET"] = "some-bucket"
+    settings.ENROLMENT_AUTHORISATION_BUCKET = "some-bucket"
     enrolment = repo.save_enrolment(course_id="123", student_id="abc")
 
     # TODO: assert enrollment is of the appropriate domain model type
@@ -41,5 +41,5 @@ def test_save_enrolment(boto_client, uuid4):
     boto_client.return_value.put_object.assert_called_once_with(
         Body=bytes(enrolment.json(), "utf-8"),
         Key=f"{enrolment.uuid}.json",  # NOQA
-        Bucket="put-enrolment-auth-bucket-here",
+        Bucket="some-bucket",
     )
