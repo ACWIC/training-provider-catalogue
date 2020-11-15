fbc1c3aeb3a1bb65221f4c5bac75bec2c936bfa1
Baris Sari <sariba@itu.edu.tr>
2020-11-15 17:27:10 +0300

    Increase coverage, add response tests

--- app/repositories/course_repo.py
+++ app/repositories/course_repo.py
@@ -6,4 +6,4 @@ from app.domain.entities.course_filters import CourseFilters
 class CourseRepo(abc.ABC):
     @abc.abstractmethod
     def search_course(self, course_filters: CourseFilters) -> dict:
-        pass
+        """"""
